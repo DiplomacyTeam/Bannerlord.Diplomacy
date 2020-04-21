5fc470308e50c4ae6bdb6bc75e1192c7a82c976a
Katarn2000x <lester.c.steve@gmail.com>
2020-04-21 03:44:59 -0700

    Adding default value text to the descriptions on the sttings page.

--- Settings.cs
+++ Settings.cs
@@ -11,23 +11,23 @@ namespace DiplomacyFixes
 
         public override string ModName { get; } = "Diplomacy Fixes";
 
-        [SettingProperty("Enable Flat Influence Costs For Diplomacy Actions", false, "If disabled, this removes all costs for diplomacy actions.")]
+        [SettingProperty("Enable Flat Influence Costs For Diplomacy Actions", false, "If disabled, this removes all costs for diplomacy actions. Default value is true.")]
         [SettingPropertyGroup("Kingdom Diplomacy Settings", true)]
         public bool EnableInfluenceCostsForDiplomacyActions { get; set; } = true;
 
-        [SettingProperty("Enable Scaling Influence Cost", false, "If enabled, this will scale influence costs based on your kingdom size.")]
+        [SettingProperty("Enable Scaling Influence Cost", false, "If enabled, this will scale influence costs based on your kingdom size. Default value is true.")]
         [SettingPropertyGroup("Kingdom Diplomacy Settings", false)]
         public bool ScalingInfluenceCosts { get; set; } = true;
 
-        [SettingProperty("Scaling Influence Cost Multiplier", 0f, 100f, false, "Multiplier for the scaling influence costs.")]
+        [SettingProperty("Scaling Influence Cost Multiplier", 0f, 100f, false, "Multiplier for the scaling influence costs. Default value is 5.")]
         [SettingPropertyGroup("Kingdom Diplomacy Settings", false)]
         public float ScalingInfluenceCostMultiplier { get; set; } = 5.0f;
 
-        [SettingProperty("Flat Declare War Influence Cost", 0, 10000, false, "Influence cost for declaring war on another kingdom.")]
+        [SettingProperty("Flat Declare War Influence Cost", 0, 10000, false, "Influence cost for declaring war on another kingdom. Default value is 100.")]
         [SettingPropertyGroup("Kingdom Diplomacy Settings", false)]
         public int DeclareWarInfluenceCost { get; set; } = 100;
 
-        [SettingProperty("Flat Make Peace Influence Cost", 0, 10000, false, "Influence cost for making peace with another kingdom.")]
+        [SettingProperty("Flat Make Peace Influence Cost", 0, 10000, false, "Influence cost for making peace with another kingdom. Default value is 100.")]
         [SettingPropertyGroup("Kingdom Diplomacy Settings", false)]
 
         public int MakePeaceInfluenceCost { get; set; } = 100;
