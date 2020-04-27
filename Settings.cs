ee024c1ef688b11c535f837cac5fd6f474c06093
Katarn2000x <lester.c.steve@gmail.com>
2020-04-27 16:56:16 -0700

    Updating project to develop on Bannerlord e1.3.0. Update MBOptionScreen to e1.3.0 compatible version.

--- Settings.cs
+++ Settings.cs
@@ -1,4 +1,5 @@
 ﻿using MBOptionScreen.Attributes;
+using MBOptionScreen.Attributes.v2;
 using MBOptionScreen.Settings;
 
 namespace DiplomacyFixes
@@ -11,45 +12,44 @@ namespace DiplomacyFixes
 
         public override string ModName { get; } = "Diplomacy Fixes";
 
-        [SettingProperty("Enable Player Kingdom Diplomacy Control", 0, 500, false, "Gives the player total control over their kingdom's war and peace declarations.")]
-        [SettingPropertyGroup("Kingdom Diplomacy", false)]
+        [SettingPropertyBool(displayName: "Enable Player Kingdom Diplomacy Control", Order = 0, RequireRestart = false, HintText = "Gives the player total control over their kingdom's war and peace declarations.")]
+        [SettingPropertyGroup("Kingdom Diplomacy")]
         public bool PlayerDiplomacyControl { get; set; } = true;
 
-        [SettingProperty("Enable Influence Costs", false, "If disabled, this removes all costs for war and peace declaration actions. Default value is true.")]
-        [SettingPropertyGroup("Influence Costs", true)]
+        [SettingPropertyBool(displayName: "Enable Influence Costs", RequireRestart = false, HintText = "If disabled, this removes all costs for war and peace declaration actions. Default value is true.")]
+        [SettingPropertyGroup("Influence Costs", isMainToggle: true)]
         public bool EnableInfluenceCostsForDiplomacyActions { get; set; } = true;
 
-        [SettingProperty("Enable Scaling Influence Cost", false, "If enabled, this will scale influence costs based on your kingdom size. Otherwise, flat influence costs are used. Default value is true.")]
-        [SettingPropertyGroup("Influence Costs", false)]
+        [SettingPropertyBool(displayName: "Enable Scaling Influence Cost", Order = 0, RequireRestart = false, HintText = "If enabled, this will scale influence costs based on your kingdom size. Otherwise, flat influence costs are used. Default value is true.")]
+        [SettingPropertyGroup("Influence Costs/Scaling", IsMainToggle = true)]
         public bool ScalingInfluenceCosts { get; set; } = true;
 
-        [SettingProperty("Scaling Influence Cost Multiplier", 0f, 100f, false, "Multiplier for the scaling influence costs. Default value is 5.")]
-        [SettingPropertyGroup("Influence Costs", false)]
+        [SettingPropertyFloatingInteger(displayName: "Scaling Influence Cost Multiplier", 0, 100, Order = 1, RequireRestart = false, HintText = "Multiplier for the scaling influence costs. Default value is 5.")]
+        [SettingPropertyGroup("Influence Costs/Scaling")]
         public float ScalingInfluenceCostMultiplier { get; set; } = 5.0f;
 
-        [SettingProperty("Flat Declare War Influence Cost", 0, 10000, false, "Influence cost for declaring war on another kingdom. Default value is 100.")]
-        [SettingPropertyGroup("Influence Costs", false)]
+        [SettingPropertyInteger(displayName: "Flat Declare War Influence Cost", 0, 100, Order = 2, RequireRestart = false, HintText = "Influence cost for declaring war on another kingdom. Default value is 100.")]
+        [SettingPropertyGroup("Influence Costs/Flat")]
         public int DeclareWarInfluenceCost { get; set; } = 100;
 
-        [SettingProperty("Flat Make Peace Influence Cost", 0, 10000, false, "Influence cost for making peace with another kingdom. Default value is 100.")]
-        [SettingPropertyGroup("Influence Costs", false)]
+        [SettingPropertyInteger("Flat Make Peace Influence Cost", 0, 10000, Order = 3, RequireRestart = false, HintText ="Influence cost for making peace with another kingdom. Default value is 100.")]
+        [SettingPropertyGroup("Influence Costs/Flat")]
         public int MakePeaceInfluenceCost { get; set; } = 100;
 
-        [SettingProperty("Minimum War Duration in Days", 0, 500, false, "The minimum duration (in days) that a war can last before proposing peace. Default value is 10.")]
-        [SettingPropertyGroup("Kingdom Diplomacy", false)]
+        [SettingPropertyInteger("Minimum War Duration in Days", 0, 500, Order = 1, RequireRestart = false, HintText = "The minimum duration (in days) that a war can last before proposing peace. Default value is 10.")]
+        [SettingPropertyGroup("Kingdom Diplomacy")]
         public int MinimumWarDurationInDays { get; set; } = 10;
 
-        [SettingProperty("Declare War Cooldown in Days", 0, 500, false, "The minimum duration (in days) to declare war after making peace. Default value is 10.")]
-        [SettingPropertyGroup("Kingdom Diplomacy", false)]
+        [SettingPropertyInteger("Declare War Cooldown in Days", 0, 500, Order = 2, RequireRestart = false, HintText ="The minimum duration (in days) to declare war after making peace. Default value is 10.")]
+        [SettingPropertyGroup("Kingdom Diplomacy")]
         public int DeclareWarCooldownInDays { get; set; } = 10;
 
-        [SettingProperty("Send Messenger Influence Cost", 0, 10000, false, "Influence cost for sending a messenger to another leader. Default value is 20.")]
-        [SettingPropertyGroup("Messengers", false)]
+        [SettingPropertyInteger("Send Messenger Influence Cost", 0, 10000, RequireRestart = false, HintText = "Influence cost for sending a messenger to another leader. Default value is 20.")]
+        [SettingPropertyGroup("Messengers")]
         public int SendMessengerInfluenceCost { get; set; } = 100;
 
-        [SettingProperty("Messenger Travel Time in Days", 0, 500, false, "The amount of time (in days) a messenger takes to reach a kingdom's leader. Default value is 3.")]
-        [SettingPropertyGroup("Messengers", false)]
+        [SettingPropertyInteger("Messenger Travel Time in Days", 0, 500, RequireRestart = false, HintText = "The amount of time (in days) a messenger takes to reach a kingdom's leader. Default value is 3.")]
+        [SettingPropertyGroup("Messengers")]
         public int MessengerTravelTime { get; set; } = 3;
-
     }
 }
---
+++ WarExhaustionManager.cs
@@ -0,0 +1,139 @@
+﻿using HarmonyLib;
+using StoryMode.StoryModePhases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace DiplomacyFixes
+{
+    class WarExhaustionManager
+    {
+        private static WarExhaustionManager _instance;
+        private Dictionary<HashSet<Kingdom>, float> _warExhaustion;
+        private HashSet<HashSet<Kingdom>> _knownKingdomCombinations;
+        private HashSet<Kingdom> _knownKingdoms;
+
+        private const float MaxWarExhaustion = 100f;
+        private const float MinWarExhaustion = 0f;
+
+        public static WarExhaustionManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new WarExhaustionManager();
+                }
+                return _instance;
+            }
+        }
+
+        private WarExhaustionManager()
+        {
+            this._warExhaustion = new Dictionary<HashSet<Kingdom>, float>(HashSet<Kingdom>.CreateSetComparer());
+            HashSet<HashSet<Kingdom>> combinations = new HashSet<HashSet<Kingdom>>(HashSet<Kingdom>.CreateSetComparer());
+            combinations.UnionWith(
+                from item1 in Kingdom.All
+                from item2 in Kingdom.All
+                where String.Compare(item1.Name.ToString(), item2.Name.ToString()) < 0
+                select new HashSet<Kingdom>() { item1, item2 });
+            this._knownKingdoms = new HashSet<Kingdom>(Kingdom.All);
+            this._knownKingdomCombinations = combinations;
+        }
+
+        public float GetWarExhaustion(Kingdom kingdom1, Kingdom kingdom2)
+        {
+            if (_warExhaustion.TryGetValue(new HashSet<Kingdom>() { kingdom1, kingdom2 }, out float warExhaustion))
+            {
+                return warExhaustion;
+            }
+            else
+            {
+                return 0f;
+            }
+        }
+
+        private void AddWarExhaustion(HashSet<Kingdom> kingdoms)
+        {
+            if (_warExhaustion.TryGetValue(kingdoms, out float currentValue))
+            {
+                _warExhaustion[kingdoms] = MBMath.ClampFloat(currentValue += Settings.Instance.WarExhaustionMultiplier, MinWarExhaustion, MaxWarExhaustion);
+            }
+            else
+            {
+                _warExhaustion[kingdoms] = MBMath.ClampFloat(Settings.Instance.WarExhaustionMultiplier, MinWarExhaustion, MaxWarExhaustion);
+            }
+        }
+
+        private void RemoveWarExhaustion(HashSet<Kingdom> kingdoms)
+        {
+            if (_warExhaustion.TryGetValue(kingdoms, out float currentValue))
+            {
+                _warExhaustion[kingdoms] = MBMath.ClampFloat(currentValue -= Settings.Instance.WarExhaustionMultiplier, MinWarExhaustion, MaxWarExhaustion);
+            }
+        }
+
+        public void UpdateWarExhaustionForAllKingdoms()
+        {
+            UpdateKnownKingdoms();
+
+            foreach (HashSet<Kingdom> kingdoms in _knownKingdomCombinations)
+            {
+                UpdateWarExhaustion(kingdoms);
+            }
+        }
+
+        private void UpdateKnownKingdoms()
+        {
+            IEnumerable<Kingdom> kingdomsToAdd = Kingdom.All.Except(_knownKingdoms);
+            if (kingdomsToAdd.Any())
+            {
+                _knownKingdomCombinations.UnionWith(
+                from item1 in Kingdom.All
+                from item2 in Kingdom.All
+                where String.Compare(item1.Name.ToString(), item2.Name.ToString()) < 0
+                select new HashSet<Kingdom>() { item1, item2 });
+                _knownKingdoms.UnionWith(kingdomsToAdd);
+            }
+        }
+
+        private void UpdateWarExhaustion(HashSet<Kingdom> kingdoms)
+        {
+            if (kingdoms.Count != 2)
+            {
+                throw new MBIllegalValueException("Can only update war exhaustion when there are two kingdoms provided.");
+            }
+
+            if (!IsValidQuestState(kingdoms))
+            {
+                return;
+            }
+
+            if (FactionManager.IsAtWarAgainstFaction(kingdoms.First(), kingdoms.First(element => element != kingdoms.First())))
+            {
+                AddWarExhaustion(kingdoms);
+            }
+            else
+            {
+                RemoveWarExhaustion(kingdoms);
+            }
+        }
+
+        private bool IsValidQuestState(HashSet<Kingdom> kingdoms)
+        {
+            bool isValidQuestState = true;
+            if (kingdoms.Any(kingdom => kingdom == Hero.MainHero.MapFaction))
+            {
+                ThirdPhase thirdPhase = StoryMode.StoryMode.Current.MainStoryLine.ThirdPhase;
+                isValidQuestState = thirdPhase == null || thirdPhase.OppositionKingdoms.Intersect(kingdoms).IsEmpty();
+            }
+            return isValidQuestState;
+        }
+    }
+}
