6243f96229cd2a12f6db6bf10332955e885b7976
Katarn2000x <lester.c.steve@gmail.com>
2020-05-02 01:49:23 -0700

    Attempt to fix bug where declare war cooldowns could be circumvented by AI factions.

---
+++ Patches/DiplomaticBartersBehaviorPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors.BarterBehaviors;
+
+namespace DiplomacyFixes.Patches
+{
+    [HarmonyPatch(typeof(DiplomaticBartersBehavior))]
+    class DiplomaticBartersBehaviorPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("ConsiderWar")]
+        public static bool ConsiderWarPatch(IFaction mapFaction, IFaction otherMapFaction)
+        {
+            return !CooldownManager.HasDeclareWarCooldown(mapFaction, otherMapFaction);
+        }
+    }
+}
