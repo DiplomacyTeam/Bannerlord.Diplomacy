ef2b9fed500997bb6ee8616930403ca3e576fe7c
Katarn2000x <lester.c.steve@gmail.com>
2020-07-11 15:42:46 -0700

    Clean up support kingdom quests when advancing the story by usurping a throne.

---
+++ Patches/SupportKingdomQuestPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using StoryMode.Behaviors.Quests;
+using System.Reflection;
+using TaleWorlds.CampaignSystem;
+
+namespace DiplomacyFixes.Patches
+{
+    [HarmonyPatch]
+    class SupportKingdomQuestPatch
+    {
+        [HarmonyTargetMethod]
+        static MethodBase TargetMethod()
+        {
+            return typeof(SupportKingdomQuestBehavior).GetNestedType("SupportKingdomQuest", BindingFlags.NonPublic | BindingFlags.Instance).GetMethod("MainStoryLineChosen", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        [HarmonyPostfix]
+        public static void PostFix(object __instance)
+        {
+            QuestBase questBase = ((QuestBase)__instance);
+            if (!questBase.IsFinalized)
+            {
+                typeof(QuestBase).GetMethod("CompleteQuestWithSuccess", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(questBase, new object[] { });
+            }
+        }
+    }
+}
