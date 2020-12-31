befc39f02039d1c0baeb130c6f1b8bce21905dd2
Matthew D. Hall <azrinon@gmail.com>
2020-12-30 16:30:41 -0800

    differentiated the saveabletypedefiner a bit (new base ID, etc.)

---
+++ src/CustomSavedTypeDefiner.cs
@@ -0,0 +1,40 @@
+﻿using Diplomacy.DiplomaticAction;
+using Diplomacy.Messengers;
+
+using System.Collections.Generic;
+
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.SaveSystem;
+
+namespace Diplomacy
+{
+    internal class CustomSavedTypeDefiner : SaveableTypeDefiner
+    {
+        private const int SaveBaseId = 1_984_110_150;
+
+        public CustomSavedTypeDefiner() : base(SaveBaseId) { }
+
+        protected override void DefineClassTypes()
+        {
+            AddClassDefinition(typeof(Messenger), 1);
+            AddClassDefinition(typeof(WarExhaustionManager), 2);
+            AddClassDefinition(typeof(CooldownManager), 3);
+            AddClassDefinition(typeof(MessengerManager), 4);
+            AddClassDefinition(typeof(DiplomaticAgreement), 5);
+            AddClassDefinition(typeof(NonAggressionPactAgreement), 6);
+            AddClassDefinition(typeof(DiplomaticAgreementManager), 7);
+            AddClassDefinition(typeof(ExpansionismManager), 8);
+        }
+
+        protected override void DefineStructTypes() => AddStructDefinition(typeof(FactionMapping), 9);
+
+        protected override void DefineContainerDefinitions()
+        {
+            ConstructContainerDefinition(typeof(List<Messenger>));
+            ConstructContainerDefinition(typeof(Dictionary<IFaction, CampaignTime>));
+            ConstructContainerDefinition(typeof(Dictionary<Kingdom, CampaignTime>));
+            ConstructContainerDefinition(typeof(List<DiplomaticAgreement>));
+            ConstructContainerDefinition(typeof(Dictionary<FactionMapping, List<DiplomaticAgreement>>));
+        }
+    }
+}
