a68602ecd9e25f2f4d059356a2aaa3155f005866
Matthew D. Hall <azrinon@gmail.com>
2020-12-31 16:49:22 -0800

    event dispatch system now works with C# 9

--- src/Events.cs
+++ src/Events.cs
@@ -1,13 +1,15 @@
 ﻿using Diplomacy.DiplomaticAction.Alliance;
 using Diplomacy.DiplomaticAction.WarPeace;
+
 using System.Collections.Generic;
+
 using TaleWorlds.CampaignSystem;
 
 namespace Diplomacy
 {
-    class Events
+    internal sealed class Events
     {
-        public static Events Instance { get; set; }
+        public static Events Instance { get; set; } = default!;
 
         private readonly MbEvent<Hero> _messengerSent = new MbEvent<Hero>();
         private readonly MbEvent<Kingdom> _peaceProposalSent = new MbEvent<Kingdom>();
@@ -16,7 +18,7 @@ namespace Diplomacy
         private readonly MbEvent<AllianceEvent> _allianceBroken = new MbEvent<AllianceEvent>();
         private readonly MbEvent<Settlement> _playerSettlementTaken = new MbEvent<Settlement>();
         private readonly MbEvent<WarDeclaredEvent> _warDeclared = new MbEvent<WarDeclaredEvent>();
-        private List<object> _listeners;
+        private readonly List<object> _listeners;
 
         public Events()
         {
@@ -32,101 +34,35 @@ namespace Diplomacy
             };
         }
 
-        public static IMbEvent<AllianceEvent> AllianceFormed
-        {
-            get
-            {
-                return Instance._allianceFormed;
-            }
-        }
+        public static IMbEvent<AllianceEvent> AllianceFormed => Instance._allianceFormed;
 
-        public static IMbEvent<AllianceEvent> AllianceBroken
-        {
-            get
-            {
-                return Instance._allianceBroken;
-            }
-        }
+        public static IMbEvent<AllianceEvent> AllianceBroken => Instance._allianceBroken;
 
-        public static IMbEvent<Hero> MessengerSent
-        {
-            get
-            {
-                return Instance._messengerSent;
-            }
-        }
+        public static IMbEvent<Hero> MessengerSent => Instance._messengerSent;
 
-        public static IMbEvent<Kingdom> PeaceProposalSent
-        {
-            get
-            {
-                return Instance._peaceProposalSent;
-            }
-        }
+        public static IMbEvent<Kingdom> PeaceProposalSent => Instance._peaceProposalSent;
 
-        public static IMbEvent<Town> FiefGranted
-        {
-            get
-            {
-                return Instance._fiefGranted;
-            }
-        }
+        public static IMbEvent<Town> FiefGranted => Instance._fiefGranted;
 
-        public static IMbEvent<Settlement> PlayerSettlementTaken
-        {
-            get
-            {
-                return Instance._playerSettlementTaken;
-            }
-        }
+        public static IMbEvent<Settlement> PlayerSettlementTaken => Instance._playerSettlementTaken;
 
-        public static IMbEvent<WarDeclaredEvent> WarDeclared
-        {
-            get
-            {
-                return Instance._warDeclared;
-            }
-        }
+        public static IMbEvent<WarDeclaredEvent> WarDeclared => Instance._warDeclared;
 
-        internal void OnMessengerSent(Hero hero)
-        {
-            Instance._messengerSent.Invoke(hero);
-        }
+        internal void OnMessengerSent(Hero hero) => Instance._messengerSent.Invoke(hero);
 
-        internal void OnPeaceProposalSent(Kingdom kingdom)
-        {
-            Instance._peaceProposalSent.Invoke(kingdom);
-        }
+        internal void OnPeaceProposalSent(Kingdom kingdom) => Instance._peaceProposalSent.Invoke(kingdom);
 
-        internal void OnFiefGranted(Town fief)
-        {
-            Instance._fiefGranted.Invoke(fief);
-        }
+        internal void OnFiefGranted(Town fief) => Instance._fiefGranted.Invoke(fief);
 
-        internal void OnAllianceFormed(AllianceEvent allianceEvent)
-        {
-            Instance._allianceFormed.Invoke(allianceEvent);
-        }
+        internal void OnAllianceFormed(AllianceEvent allianceEvent) => Instance._allianceFormed.Invoke(allianceEvent);
 
-        internal void OnAllianceBroken(AllianceEvent allianceEvent)
-        {
-            Instance._allianceBroken.Invoke(allianceEvent);
-        }
+        internal void OnAllianceBroken(AllianceEvent allianceEvent) => Instance._allianceBroken.Invoke(allianceEvent);
 
-        internal void OnPlayerSettlementTaken(Settlement currentSettlement)
-        {
-            Instance._playerSettlementTaken.Invoke(currentSettlement);
-        }
+        internal void OnPlayerSettlementTaken(Settlement currentSettlement) => Instance._playerSettlementTaken.Invoke(currentSettlement);
 
-        internal void OnWarDeclared(WarDeclaredEvent warDeclaredEvent)
-        {
-            Instance._warDeclared.Invoke(warDeclaredEvent);
-        }
+        internal void OnWarDeclared(WarDeclaredEvent warDeclaredEvent) => Instance._warDeclared.Invoke(warDeclaredEvent);
 
-        public static void RemoveListeners(object o)
-        {
-            Events.Instance.RemoveListenersInternal(o);
-        }
+        public static void RemoveListeners(object o) => Instance.RemoveListenersInternal(o);
 
         internal void RemoveListenersInternal(object obj)
         {
