f8f7c7b41e9919ddf92895821d458050d6d9b2c0
Matthew D. Hall <azrinon@gmail.com>
2021-01-02 16:59:50 -0800

    kingdom screen compatibility crash fix

--- src/Patches/KingdomManagementVMPatch.cs
+++ src/Patches/KingdomManagementVMPatch.cs
@@ -1,7 +1,10 @@
 ﻿using Diplomacy.ViewModel;
+
 using HarmonyLib;
+
 using System;
 using System.Reflection;
+
 using TaleWorlds.CampaignSystem.Election;
 using TaleWorlds.CampaignSystem.ViewModelCollection;
 using TaleWorlds.CampaignSystem.ViewModelCollection.KingdomManagement;
@@ -11,33 +14,35 @@ using TaleWorlds.CampaignSystem.ViewModelCollection.KingdomManagement.KingdomDip
 namespace Diplomacy.Patches
 {
     [HarmonyPatch(typeof(KingdomManagementVM))]
-    class KingdomManagementVMPatch
+    internal static class KingdomManagementVMPatch
     {
         [HarmonyPostfix]
         [HarmonyPatch("RefreshValues")]
-        public static void RefreshValuesPatch(KingdomManagementVM __instance)
+        static void RefreshValuesPatch(KingdomManagementVM __instance)
         {
+            // FIXME: Upgrade all this uncached reflection to use cached delegates
             var forceDecideDecision = __instance.GetType().GetMethod("ForceDecideDecision", BindingFlags.NonPublic | BindingFlags.Instance);
             var forceDecideDecisionAction = (Action<KingdomDecision>)Delegate.CreateDelegate(typeof(Action<KingdomDecision>), __instance, forceDecideDecision);
             __instance.Clan = new KingdomClanVMExtensionVM(forceDecideDecisionAction);
             __instance.Diplomacy = new KingdomDiplomacyVMExtensionVM(forceDecideDecisionAction);
 
-            var currentCategoryFieldInfo =
-                (KingdomCategoryVM)typeof(KingdomManagementVM).GetField("_currentCategory", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
-            var setCurrentCategoryMethodInfo = __instance.GetType().GetMethod("SetCurrentCategory", BindingFlags.NonPublic | BindingFlags.Instance);
-            if (currentCategoryFieldInfo is KingdomClanVM)
-            {
+            int currentCategoryFieldInfo = (int)typeof(KingdomManagementVM)
+                .GetField("_currentCategory", BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue(__instance);
+
+            var setCurrentCategoryMethodInfo = __instance.GetType()
+                .GetMethod("SetSelectedCategory", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            // FIXME: Import whatever private enum these numbers stand for
+            if (currentCategoryFieldInfo == 0)
                 setCurrentCategoryMethodInfo.Invoke(__instance, new object[] { __instance.Clan });
-            }
-            else if (currentCategoryFieldInfo is KingdomDiplomacyVM)
-            {
+            else if (currentCategoryFieldInfo == 4)
                 setCurrentCategoryMethodInfo.Invoke(__instance, new object[] { __instance.Diplomacy });
-            }
         }
 
         [HarmonyPrefix]
         [HarmonyPatch("ExecuteClose")]
-        public static void ExecuteClosePatch(KingdomManagementVM __instance)
+        static void ExecuteClosePatch(KingdomManagementVM __instance)
         {
             ((KingdomDiplomacyVMExtensionVM)__instance.Diplomacy).OnClose();
             ((KingdomClanVMExtensionVM)__instance.Clan).OnClose();
