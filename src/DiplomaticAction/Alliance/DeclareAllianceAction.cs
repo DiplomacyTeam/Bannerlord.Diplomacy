c32ac1322f84ee91ca691aa401fc85f986c3a635
Matthew D. Hall <azrinon@gmail.com>
2021-01-02 16:37:37 -0800

    fancy version of jiro's alliance fix

--- src/DiplomaticAction/Alliance/DeclareAllianceAction.cs
+++ src/DiplomaticAction/Alliance/DeclareAllianceAction.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Bannerlord.ButterLib.Common.Helpers;
+
+using Microsoft.Extensions.Logging;
 
 using System;
+
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.Localization;
@@ -10,21 +13,17 @@ namespace Diplomacy.DiplomaticAction.Alliance
     internal sealed class DeclareAllianceAction : AbstractDiplomaticAction<DeclareAllianceAction>
     {
         public override bool PassesConditions(Kingdom kingdom, Kingdom otherKingdom, bool forcePlayerCosts = false, bool bypassCosts = false)
-        {
-            return FormAllianceConditions.Instance.CanApply(kingdom, kingdom, forcePlayerCosts, bypassCosts);
-        }
+            => FormAllianceConditions.Instance.CanApply(kingdom, kingdom, forcePlayerCosts, bypassCosts);
 
         protected override void ApplyInternal(Kingdom proposingKingdom, Kingdom otherKingdom, float? customDurationInDays)
         {
             LogFactory.Get<DeclareAllianceAction>().LogTrace($"[{CampaignTime.Now}] {proposingKingdom.Name} secured an alliance with {otherKingdom.Name}.");
-            FactionManager.DeclareAlliance(proposingKingdom, otherKingdom);
+            DeclareAlliance(proposingKingdom, otherKingdom);
             Events.Instance.OnAllianceFormed(new AllianceEvent(proposingKingdom, otherKingdom));
         }
 
         protected override void AssessCosts(Kingdom proposingKingdom, Kingdom otherKingdom, bool forcePlayerCosts)
-        {
-            DiplomacyCostCalculator.DetermineCostForFormingAlliance(proposingKingdom, otherKingdom, forcePlayerCosts).ApplyCost();
-        }
+            => DiplomacyCostCalculator.DetermineCostForFormingAlliance(proposingKingdom, otherKingdom, forcePlayerCosts).ApplyCost();
 
         protected override void ShowPlayerInquiry(Kingdom proposingKingdom, Action acceptAction)
         {
@@ -42,5 +41,24 @@ namespace Diplomacy.DiplomaticAction.Alliance
                 acceptAction,
                 null), true);
         }
+
+        private static void DeclareAlliance(IFaction faction1, IFaction faction2)
+        {
+            if (faction1 == faction2 || faction1.IsBanditFaction || faction2.IsBanditFaction)
+                return;
+
+            SetStance(faction1, faction2, StanceType.Alliance);
+        }
+
+        enum StanceType
+        {
+            Neutral,
+            War,
+            Alliance,
+        }
+
+        private delegate StanceLink SetStanceDel(IFaction faction1, IFaction faction2, StanceType stanceType);
+        private static readonly SetStanceDel SetStance = AccessTools2.GetDelegate<SetStanceDel>(typeof(FactionManager), "SetStance")
+            ?? throw new MissingMethodException(typeof(FactionManager).FullName, "SetStance");
     }
 }
