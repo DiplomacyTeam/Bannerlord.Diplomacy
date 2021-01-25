48b17d1181313bf1a0e0d1e8ad3800e618700912
Matthew D. Hall <azrinon@gmail.com>
2021-01-24 17:39:00 -0800

    fixed-up DefaultClanPoliticsModelPatch for e1.5.7

--- TODO.md
+++ TODO.md
@@ -3,26 +3,48 @@
 
 ## Higher Priority
 
+
 #### Compatibility with BL e1.5.7
 
 - ~~GUI compatibility~~
+
 - Compile-time compatibility
+
   + End usage of `StatExplainer`
+
 - Harmony patch compatibility
+
   + Full review of all patched methods for semantic correctness
 
 
 ## Normal Priority
 
 
+- Convert all Harmony patches from annotated/attribute form to declarative form (actually using `Harmony.Patch` for each patch, except with very convenient instrumentation)
+
+  + Why? To make it safe to build Diplomacy with full compiler and JIT optimizations enabled as well as potentially detect patching conflicts on the fly, providing at the very least better diagnostics
+
+
+- Create a special `CampaignLog` logger which outputs a plaintext log of campaign events/actions/outcomes as well as some light statistics.
+
+  + Since the goal is to be as readable as possible, all entries will be timestamped with campaign time, and we don't want to impose performance overhead from it (i.e., avoiding "autoflush" behavior), this will mostly likely use a custom asynchronous file I/O backend for logging instead of the Serilog from ButterLib used in the rest of the code.
+
+  + However, since sometimes we'll want to log something to both the Serilog provider and to the `CampaignLog`, it should provide an extension for logging to both and handling the different formatting appropriately.
+
+
 #### Deactivate 'Usurp Throne' feature
 
 - Deactivation itself
+
 - Rearrangement of UI buttons to again look decent without it
 
 
 ## Lower Priority
 
+- Add logging throughout the codebase for user diagnostics and our own debugging.
+
 - Address the hundreds of nullability warnings inherited from DiplomacyFixes.
 
 - Convert complete overrides of vanilla GUI Prefabs to UIExtenderEx patches for better compatibility and more robustness to vanilla changes.
+
+- Wrap MCM settings in a larger config class
--- src/Patches/DefaultClanPoliticsModelPatch.cs
+++ src/Patches/DefaultClanPoliticsModelPatch.cs
@@ -11,46 +11,53 @@ namespace Diplomacy.Patches
     [HarmonyPatch(typeof(DefaultClanPoliticsModel))]
     class DefaultClanPoliticsModelPatch
     {
+        private static readonly TextObject _txtCorruption = new("{=dUCOV7km}Corruption: Too Many Fiefs");
+        private static readonly TextObject _txtInfluenceDecay = new("{=koTNaZUX}Decay of High Influence");
+        private static readonly TextObject _txtInfluenceLossLimit = new("{=dUCOV7km}Influence Loss Limit");
+
         [HarmonyPostfix]
+        [HarmonyPriority(Priority.Last)]
         [HarmonyPatch("CalculateInfluenceChange")]
-        public static void CalculateInfluenceChangePatch(ref float __result, Clan clan, StatExplainer? explanation = null)
+        public static void CalculateInfluenceChangePatch(Clan clan, ref ExplainedNumber __result)
         {
             if (Settings.Instance!.EnableInfluenceBalancing)
             {
-                var influenceChange = __result;
+                /// Corruption
 
                 if (Settings.Instance.EnableCorruption)
                 {
-                    var corruption = clan.GetCorruption();
-                    if (corruption > 0)
-                    {
-                        explanation?.AddLine(new TextObject("{=dUCOV7km}Corruption (too many fiefs)").ToString(), -corruption);
-                        influenceChange -= corruption;
-                    }
+                    float corruption = clan.GetCorruption();
+
+                    if (corruption >= 0.01f)
+                        __result.Add(-corruption, _txtCorruption);
                 }
 
-                if (Settings.Instance.EnableInfluenceDecay)
+                /// Influence Decay
+
+                if (Settings.Instance.EnableInfluenceDecay && clan.Influence > Settings.Instance.InfluenceDecayThreshold)
                 {
-                    var influenceDecayFactor = clan.Influence > Settings.Instance.InfluenceDecayThreshold
-                        ? -(int)((clan.Influence - Settings.Instance.InfluenceDecayThreshold) * (Settings.Instance.InfluenceDecayPercentage / 100))
-                        : 0;
+                    float decayFactor = Settings.Instance.InfluenceDecayPercentage / 100f;
+                    int decay = (int)(decayFactor * (clan.Influence - Settings.Instance.InfluenceDecayThreshold));
 
-                    if (influenceDecayFactor < 0)
-                    {
-                        explanation?.AddLine(new TextObject("{=koTNaZUX}Influence Decay (too much influence)").ToString(), influenceDecayFactor);
-                        influenceChange += influenceDecayFactor;
-                    }
+                    if (decay > 0)
+                        __result.Add(-decay, _txtInfluenceDecay);
                 }
 
-                float maximumInfluenceLoss = Settings.Instance.MaximumInfluenceLoss;
+                /// Influence Loss Limit
 
-                if (influenceChange < -maximumInfluenceLoss)
-                {
-                    explanation?.AddLine(new TextObject("{=uZc8Hg01}Maximum Influence Loss").ToString(), -maximumInfluenceLoss, StatExplainer.OperationType.LimitMin);
-                    influenceChange = -maximumInfluenceLoss;
-                }
+                int lossLimit = Settings.Instance.MaximumInfluenceLoss;
+                float total = __result.ResultNumber;
 
-                __result = influenceChange;
+                // NOTE: This used to be a LimitMin, but with e1.5.7, ExplainedNumber.LimitMin does not
+                // accept a description of the operation. IDK whether it'd provide a generic one or it'd
+                // omit it entirely, and neither seems acceptable. Since I also ensured that this postfix
+                // patch runs last in priority, I've converted the logical LimitMin operation into an Add
+                // off the offset required to enforce the minimum.
+
+                float limitDiff = -total - lossLimit;
+
+                if (limitDiff > 0)
+                    __result.Add(limitDiff, _txtInfluenceLossLimit);
             }
         }
     }
