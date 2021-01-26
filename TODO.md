69379446cc99bb1a2019e9f48805f7ed189d5c92
Matthew D. Hall <azrinon@gmail.com>
2021-01-25 19:39:20 -0800

    form_alliance console cmd & moved existing cmds out of Extensions/

--- TODO.md
+++ TODO.md
@@ -19,6 +19,11 @@
 
 ## Normal Priority
 
+- Create `ExplainedScore` class to wrap AI scoring summaries, which only stores a list of `(string, float)` tuples and a `ResultNumber` running total (with some methods, of course, incl. conversion to an actual `ExplainedNumber`)
+
+  + Currently for pre-e1.5.7, we rely upon the overqualified, very accessible `StatExplainer` so that we can later look at the list of scores and build tooltips out of them.
+
+    * That's now buried deep in private accessibility, so I judge that since we don't even want most of `ExplainedNumber` and its private, nested `StatExplainer` (and its private, nested `ExplainedLine`), it'd be more elegant to simply create only what we do want than build a totally unnecessary, reflection-heavy adapter for `ExplainedNumber`. The adapter would boil down to exposing the same basic data, and these score summaries are currently never compelled by any API to be an actual `ExplainedNumber`.
 
 - Convert all Harmony patches from annotated/attribute form to declarative form (actually using `Harmony.Patch` for each patch, except with very convenient instrumentation)
 
@@ -56,6 +61,11 @@
 
 
 ## Lower Priority
+
+
+- Convert usage of `ChangeRelationAction.ApplyPlayerRelation` to `ChangeRelationAction.ApplyRelationChangeBetweenHeroes` or a more advanced action which properly affects relatives where necessary.
+
+  + Even though these are only found for currently player-only interactions, we should never assume that anything is player-only, and we should prepare for an eventual overhaul of the relations system.
 
 - Add logging throughout the codebase for user diagnostics and our own debugging.
 
---
+++ src/Cheats.cs
@@ -0,0 +1,48 @@
+﻿using Diplomacy.DiplomaticAction.Alliance;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Library;
+
+namespace Diplomacy.Extensions
+{
+    class CampaignCheatsExtension
+    {
+        [CommandLineFunctionality.CommandLineArgumentFunction("form_alliance", "campaign")]
+        public static string FormAlliance(List<string> strings)
+        {
+            if (!CampaignCheats.CheckCheatUsage(ref CampaignCheats.ErrorType))
+                return CampaignCheats.ErrorType;
+
+            if (!CampaignCheats.CheckParameters(strings, 2) || CampaignCheats.CheckHelp(strings))
+                return "Format uses kingdom IDs without spaces: \"campaign.form_alliance [Kingdom1] [Kingdom2]\".";
+
+            var b1 = strings[0].ToLower();
+            var b2 = strings[1].ToLower();
+
+            if (b1 == b2)
+                return "Cannot ally a kingdom to itself!";
+
+            Kingdom? kingdom1 = null;
+            Kingdom? kingdom2 = null;
+
+            foreach (var k in Kingdom.All)
+            {
+                var id = k.Name.ToString().ToLower().Replace(" ", "");
+
+                if (id == b1)
+                    kingdom1 = k;
+                else if (id == b2)
+                    kingdom2 = k;
+            }
+
+            if (kingdom1 is null)
+                return "1st kingdom ID not found: " + b1;
+
+            if (kingdom2 is null)
+                return "2nd kingdom ID not found: " + b2;
+
+            DeclareAllianceAction.Apply(kingdom1, kingdom2, bypassCosts: true);
+            return $"Alliance formed between {kingdom1.Name} and  {kingdom2.Name}";
+        }
+    }
+}
