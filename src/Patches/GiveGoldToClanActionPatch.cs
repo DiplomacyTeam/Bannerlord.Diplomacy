29a80d9222c6912a04f15d9bfa25f108378e57d8
Matthew D. Hall <azrinon@gmail.com>
2021-02-09 06:05:49 -0800

    Conquest of Harmony: V

--- src/Patches/GiveGoldToClanActionPatch.cs
+++ src/Patches/GiveGoldToClanActionPatch.cs
@@ -1,18 +1,28 @@
-﻿using HarmonyLib;
-using System.Reflection;
+﻿using Diplomacy.PatchTools;
+
+using System;
+using System.Collections.Generic;
+
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
 
 namespace Diplomacy.Patches
 {
-    [HarmonyPatch(typeof(GiveGoldToClanAction))]
-    class GiveGoldToClanActionPatch
+    internal sealed class GiveGoldToClanActionPatch : PatchClass<GiveGoldToClanActionPatch>
     {
-        [HarmonyPrefix]
-        [HarmonyPatch("ApplyFromHeroToClan")]
-        public static bool ApplyFromHeroToClanPatch(Hero giverHero, Clan clan, int amount)
+        private static readonly Type TargetType = typeof(GiveGoldToClanAction); // Static type
+
+        protected override IEnumerable<Patch> Prepare() => new Patch[]
         {
-            typeof(GiveGoldToClanAction).GetMethod("ApplyInternal", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[] { giverHero, clan, amount });
+            new Prefix(nameof(ApplyFromHeroToClanReplaced), TargetType, nameof(GiveGoldToClanAction.ApplyFromHeroToClan)),
+        };
+
+        private delegate void ApplyInternalDel(Hero giverHero, Clan clan, int amount);
+        private static readonly ApplyInternalDel ApplyInternal = new Reflect.Method(TargetType, "ApplyInternal").GetDelegate<ApplyInternalDel>();
+
+        private static bool ApplyFromHeroToClanReplaced(Hero giverHero, Clan clan, int amount)
+        {
+            ApplyInternal(giverHero, clan, amount);
             return false;
         }
     }
