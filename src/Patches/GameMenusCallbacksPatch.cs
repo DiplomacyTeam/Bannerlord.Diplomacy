2f17a9a47bd0101e9b7124ec66fd1f17d49c852a
Katarn2000x <lester.c.steve@gmail.com>
2021-03-25 21:48:56 -0700

    Attempt at fix for right of first refusal by attaching to new menu callback.

--- src/Patches/GameMenusCallbacksPatch.cs
+++ src/Patches/GameMenusCallbacksPatch.cs
@@ -1,23 +1,29 @@
 ﻿using Diplomacy.PatchTools;
-
+using HarmonyLib;
 using System.Collections.Generic;
 
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 
 namespace Diplomacy.Patches
 {
-    internal sealed class GameMenusCallbacksPatch : PatchClass<GameMenusCallbacksPatch, GameMenusCallbacks>
+    internal sealed class GameMenusCallbacksPatch : PatchClass<GameMenusCallbacksPatch>
     {
         protected override IEnumerable<Patch> Prepare()
         {
 #if STABLE
             return new[]
             {
-                new Postfix(nameof(menu_settlement_taken_on_init_Postfix), nameof(GameMenusCallbacks.menu_settlement_taken_on_init))
+                new Postfix(nameof(menu_settlement_taken_on_init_Postfix), typeof(GameMenusCallbacks), nameof(GameMenusCallbacks.menu_settlement_taken_on_init))
             };
 #else
             // FIXME: Functionality is disabled until I can figure out how to replace this properly on e1.5.8
+            // var method = AccessTools.Method(typeof(SiegeAftermathCampaignBehavior), "siege_aftermath_contextual_summary_on_init").Name;
+            return new[]
+            {
+                new Postfix(nameof(menu_settlement_taken_on_init_Postfix), typeof(SiegeAftermathCampaignBehavior), "siege_aftermath_contextual_summary_on_init")
+            };
 #endif
         }
 
