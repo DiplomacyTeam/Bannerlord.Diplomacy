4d5cbbaff9dcb18616a4a890ee39668ec02a1373
Katarn2000x <lester.c.steve@gmail.com>
2021-03-27 23:18:53 -0700

    Added helper to easily update trait experience for the player character.

---
+++ src/Character/PlayerCharacterTraitEventExperience.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+
+namespace Diplomacy.Character
+{
+    internal class PlayerCharacterTraitEventExperience
+    {
+        public int Experience { get; }
+        public TraitObject Trait { get; }
+
+        public PlayerCharacterTraitEventExperience(int experience, TraitObject trait)
+        {
+            Experience = experience;
+            Trait = trait;
+        }
+
+        public void Apply()
+        {
+            PlayerCharacterTraitHelper.UpdateTrait(this);
+        }
+
+        private static Func<int, TraitObject, PlayerCharacterTraitEventExperience> Create = (a,b) => new PlayerCharacterTraitEventExperience(a, b);
+
+        public static readonly PlayerCharacterTraitEventExperience FiefGranted = Create(50, DefaultTraits.Generosity);
+        public static readonly PlayerCharacterTraitEventExperience FiefClaimed = Create(-50, DefaultTraits.Generosity);
+
+    }
+}
