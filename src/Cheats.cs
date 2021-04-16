b7b79aad3be6c1d3f4f1b889229ca55f6aaecd2b
Katarn2000x <lester.c.steve@gmail.com>
2021-04-16 01:00:14 -0700

    Added new toggle toggle ui debog mode console command.

--- src/Cheats.cs
+++ src/Cheats.cs
@@ -1,8 +1,7 @@
 ﻿using Diplomacy.DiplomaticAction.Alliance;
-
 using System.Collections.Generic;
-
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.Engine.GauntletUI;
 using TaleWorlds.Library;
 
 namespace Diplomacy.Extensions
@@ -48,6 +47,63 @@ namespace Diplomacy.Extensions
 
             DeclareAllianceAction.Apply(kingdom1, kingdom2, bypassCosts: true);
             return $"Alliance formed between {kingdom1.Name} and  {kingdom2.Name}!";
+        }
+
+        [CommandLineFunctionality.CommandLineArgumentFunction("set_war_exhaustion", "diplomacy")]
+        private static string SetWarExhaustion(List<string> strings)
+        {
+            bool isNumeric = int.TryParse(strings[0], out int warExhaustion);
+
+            if (!CampaignCheats.CheckParameters(strings, 3) || CampaignCheats.CheckHelp(strings) || !isNumeric)
+                return "Format uses 2 kingdom ID parameters without spaces and a war exhaustion integer: diplomacy.form_alliance [Kingdom1] [Kingdom2] [int]";
+
+            var b1 = strings[1].ToLower();
+            var b2 = strings[2].ToLower();
+
+            if (b1 == b2)
+                return "Cannot have war exhaustion with itself!";
+
+            Kingdom? kingdom1 = null;
+            Kingdom? kingdom2 = null;
+
+            foreach (var k in Kingdom.All)
+            {
+                var id = k.Name.ToString().ToLower().Replace(" ", "");
+
+                if (id == b1)
+                    kingdom1 = k;
+                else if (id == b2)
+                    kingdom2 = k;
+            }
+
+            if (kingdom1 is null && kingdom2 is null)
+                return "Could not find either required kingdom!";
+
+            if (kingdom1 is null)
+                return "1st kingdom ID not found: " + b1;
+
+            if (kingdom2 is null)
+                return "2nd kingdom ID not found: " + b2;
+
+            var current = WarExhaustionManager.Instance.GetWarExhaustion(kingdom1, kingdom2);
+            var toAdd = (warExhaustion - current) / Settings.Instance!.WarExhaustionPerCasualty;
+
+            WarExhaustionManager.Instance.AddCasualtyWarExhaustion(kingdom1, kingdom2, (int)toAdd);
+            return "done!";
+        }
+
+        [CommandLineFunctionality.CommandLineArgumentFunction("toggle_debug_mode", "ui")]
+        public static string ToggleUIDebugMode(List<string> strings)
+        {
+            UIConfig.DebugModeEnabled = !UIConfig.DebugModeEnabled;
+            return "UI Debug Mode " + (UIConfig.DebugModeEnabled ? "Enabled" : "Disabled");
+        }
+
+        [CommandLineFunctionality.CommandLineArgumentFunction("reload", "ui")]
+        public static string ReloadUI(List<string> strings)
+        {
+            UIResourceManager.Update();
+            return "Reloaded";
         }
     }
 }
