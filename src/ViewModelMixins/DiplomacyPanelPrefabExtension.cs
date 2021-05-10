99749f6f52ff7b15dc9581c9f25005a9bf47cfa5
Katarn2000x <lester.c.steve@gmail.com>
2021-05-10 14:02:21 -0700

    Added support for versioned DiplomacyPanel XMLs.

---
+++ src/ViewModelMixins/DiplomacyPanelPrefabExtension.cs
@@ -0,0 +1,33 @@
+﻿using Bannerlord.UIExtenderEx.Attributes;
+using Bannerlord.UIExtenderEx.Prefabs2;
+using System.Xml;
+
+namespace Diplomacy.ViewModelMixins
+{
+    [PrefabExtension("DiplomacyPanel", "descendant::Widget[@IsVisible='@Show']")]
+    internal sealed class DiplomacyPanelPrefabExtension : PrefabExtensionInsertPatch
+    {
+        public override InsertType Type => InsertType.Replace;
+
+        // The file should have an extension of type .xml, and be located inside of the GUI folder of your module.
+        // You can include or omit the extension type. I.e. both of the following would work:
+        //   ExampleFileInjectedPatch
+        //   ExampleFileInjectedPatch.xml
+
+        private XmlDocument document;
+
+        public DiplomacyPanelPrefabExtension()
+        {
+            document = new XmlDocument();
+#if e159
+            document.LoadXml(@"<DiplomacyPanel_159 />");
+#elif e1510
+            document.LoadXml(@"<DiplomacyPanel_1510 />");
+#endif
+        }
+
+        [PrefabExtensionXmlDocument]
+        public XmlDocument GetPrefabExtension() => document;
+
+    }
+}
