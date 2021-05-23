ee9843cbf1a20580f7aefbde9dafb8401f5cb80b
Katarn2000x <lester.c.steve@gmail.com>
2021-05-23 02:45:48 -0700

    Changed to a more stable method of denying the kingdom screen to rebellion participant players.

--- src/Patches/RebelKingdomPatches.cs
+++ src/Patches/RebelKingdomPatches.cs
@@ -1,11 +1,8 @@
 ﻿using Diplomacy.Extensions;
 using Diplomacy.PatchTools;
-using SandBox;
-using SandBox.View.Map;
 using System;
 using System.Collections.Generic;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.Localization;
 
 namespace Diplomacy.Patches
 {
@@ -20,16 +17,7 @@ namespace Diplomacy.Patches
             new Postfix(nameof(EnforceWarConditions), conversationBehaviorType, "conversation_player_wants_to_make_peace_on_condition"),
             new Postfix(nameof(EnforceWarConditions), conversationBehaviorType, "conversation_lord_request_mission_ask_on_condition"),
             new Postfix(nameof(EnforceWarConditions), conversationBehaviorType, "conversation_player_want_to_join_faction_as_mercenary_or_vassal_on_condition"),
-            new Postfix(nameof(DisableKingdomScreen), typeof(MapNavigationHandler), "get_KingdomPermission")
                     };
-        }
-
-        private static void DisableKingdomScreen(ref NavigationPermissionItem __result)
-        {
-            if (__result.IsAuthorized && Clan.PlayerClan.Kingdom != null && Clan.PlayerClan.Kingdom.IsRebelKingdom())
-            {
-                __result = new NavigationPermissionItem(false, new TextObject("{=f66sNaiz}You cannot be part of a rebellion"));
-            }
         }
 
         private static void EnforceWarConditions(ref bool __result)
---
+++ src/ViewModelMixins/MapNavigationVMMixin.cs
@@ -0,0 +1,26 @@
+﻿using Bannerlord.UIExtenderEx.Attributes;
+using Bannerlord.UIExtenderEx.ViewModels;
+using Diplomacy.Extensions;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Map;
+using TaleWorlds.Localization;
+
+namespace Diplomacy.ViewModelMixins
+{
+    [ViewModelMixin("RefreshPermissionValues")]
+    internal sealed class MapNavigationVMMixin : BaseViewModelMixin<MapNavigationVM>
+    {
+        private static readonly TextObject _TRebelKingdomText = new TextObject("{=f66sNaiz}You cannot be part of a rebellion");
+
+        public MapNavigationVMMixin(MapNavigationVM vm) : base(vm) { }
+
+        public override void OnRefresh()
+        {
+            if (Clan.PlayerClan?.Kingdom.IsRebelKingdom() ?? false)
+            {
+                ViewModel!.IsKingdomEnabled = false;
+                ViewModel!.KingdomHint.SetHintCallback(() => _TRebelKingdomText.ToString());
+            }
+        }
+    }
+}
