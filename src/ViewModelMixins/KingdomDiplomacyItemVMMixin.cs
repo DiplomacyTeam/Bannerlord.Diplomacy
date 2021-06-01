68e3bfac547cecf76d6a2f84cffd5f1760d88214
Katarn2000x <lester.c.steve@gmail.com>
2021-05-31 23:10:30 -0700

    Updated the Diplomacy screen to word wrap properly, rearranged the action buttons, and removed the send messenger button.

---
+++ src/ViewModelMixins/KingdomDiplomacyItemVMMixin.cs
@@ -0,0 +1,31 @@
+﻿using Bannerlord.UIExtenderEx.Attributes;
+using Bannerlord.UIExtenderEx.ViewModels;
+using Diplomacy.ViewModel;
+using TaleWorlds.Library;
+
+namespace Diplomacy.ViewModelMixins
+{
+    [ViewModelMixin]
+    internal sealed class KingdomTruceItemVMMixin : BaseViewModelMixin<KingdomTruceItemVMExtensionVM>
+    {
+        public KingdomTruceItemVMMixin(KingdomTruceItemVMExtensionVM vm) : base(vm)
+        {
+            IsWarItem = false;
+        }
+
+        [DataSourceProperty]
+        public bool IsWarItem { get; }
+    }
+
+    [ViewModelMixin]
+    internal sealed class KingdomWarItemVMMixin : BaseViewModelMixin<KingdomWarItemVMExtensionVM>
+    {
+        public KingdomWarItemVMMixin(KingdomWarItemVMExtensionVM vm) : base(vm)
+        {
+            IsWarItem = true;
+        }
+
+        [DataSourceProperty]
+        public bool IsWarItem { get; }
+    }
+}
