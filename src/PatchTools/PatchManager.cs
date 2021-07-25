66024d5ca3e526dfa9d04c8fe16f42c6ce55f470
Katarn2000x <lester.c.steve@gmail.com>
2021-07-24 23:38:18 -0700

    Ensuring that finalize is called on DiplomacyVM mixin.

--- src/PatchTools/PatchManager.cs
+++ src/PatchTools/PatchManager.cs
@@ -69,7 +69,8 @@ namespace Diplomacy.PatchTools
             new SupportKingdomQuestPatch(),
             new FactionManagerPatch(),
             new DefaultEncyclopediaFactionPagePatch(),
-            new RebelKingdomPatches()
+            new RebelKingdomPatches(),
+            new KingdomManagementVMPatch()
             // ... Only 1 class left to convert to declarative patching.
         };
     }
---
+++ src/Patches/KingdomManagementVMPatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Diplomacy.CivilWar.Factions;
+using Diplomacy.Extensions;
+using Diplomacy.PatchTools;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+
+namespace Diplomacy.Patches
+{
+    internal sealed class KingdomManagementVMPatch : PatchClass<KingdomManagementVMPatch>
+    {
+        protected override IEnumerable<Patch> Prepare()
+        {
+            return new Patch[]
+            {
+                new Postfix(nameof(FinalizeFix), typeof(KingdomManagementVM), "OnFinalize"),
+            };
+        }
+
+        private static void FinalizeFix(KingdomManagementVM __instance)
+        {
+            __instance.Diplomacy.OnFinalize();
+        }
+    }
+}
