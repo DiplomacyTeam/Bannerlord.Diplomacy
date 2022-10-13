e0ba364ab8ec7835df67785c2ed3fe25fa2c990d
zeltop <xyz3lt@gmail.com>
2022-10-13 23:22:04 +0200

    added new generic condition for war declaration, alliance formin, and nap forming

---
+++ src/Bannerlord.Diplomacy/DiplomaticAction/GenericConditions/NotEliminatedCondition.cs
@@ -0,0 +1,24 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Localization;
+
+namespace Diplomacy.DiplomaticAction.GenericConditions
+{
+    internal sealed class NotEliminatedCondition : IDiplomacyCondition
+    {
+        public bool ApplyCondition(Kingdom kingdom,
+                                   Kingdom otherKingdom,
+                                   out TextObject? textObject,
+                                   bool forcePlayerCosts = false,
+                                   bool bypassCosts = false)
+        {
+            textObject = null;
+
+            var eliminated = !(kingdom.IsEliminated || otherKingdom.IsEliminated);
+
+            if (!eliminated)
+                textObject = new TextObject("{=lQAaLeSy}Faction has been eliminated.");
+
+            return eliminated;
+        }
+    }
+}
